426bc3998f7ea363bbcc0cff2956dcf8d46d3417
short_horizon451/short 3 <115152760+shorthorizon451@users.noreply.github.com>
2022-10-07 16:20:18 +0800

    Add files via upload
    Only works on Windows Professional and Enterprise Versions! (tested)

---
+++ 2022-10_code.cmd
@@ -0,0 +1,33 @@
+@echo off
+color 0
+cls 
+echo H  H                  TTTTT             h           ,      DDDD          !
+echo H  H                    T               h                  D   D         !
+echo HHHH aaa ppp ppp y y    T   eee aaa ccc hhh eee rrr   ss   D   D aaa y y !
+echo H  H a a p p p p y y    T   e e a a c   h h e e r     s    D   D a a y y 
+echo H  H aaa ppp ppp yyy    T   eee aaa ccc h h eee r     sss  DDDD  aaa yyy .
+echo        a p   p     y        e     a         e           s          a   y
+echo          p   p    yy        eee             eee       sss             yy
+msg * "Happy Teacher's Day! :)"
+:y
+color 1
+color 2
+color 3
+color 4
+color 5
+color 7
+color 8
+color 9
+color a
+color b
+color c
+color d
+color e
+color f
+color 6
+color 5 
+color 4 
+color 3
+color 2 
+color 1 
+goto :y
